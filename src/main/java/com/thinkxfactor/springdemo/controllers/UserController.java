4c2e782e5941d969dffbd3f7249a2261f38ef131
Anupam Samanta <anupam96786@gmail.com>
2022-02-16 20:31:25 +0530

    userController and index.html added

---
+++ src/main/java/com/thinkxfactor/springdemo/controllers/UserController.java
@@ -0,0 +1,15 @@
+package com.thinkxfactor.springdemo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+}
