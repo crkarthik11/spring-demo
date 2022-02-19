a24d708b939323c6d92d95e13209ca7a2668ffce
CR <crkarthik11@gmail.com>
2022-02-19 14:32:43 +0530

    swagger integration

--- src/main/java/com/thinkxfactor/springdemo/SpringdemoApplication.java
+++ src/main/java/com/thinkxfactor/springdemo/SpringdemoApplication.java
@@ -2,16 +2,36 @@ package com.thinkxfactor.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-
+@ComponentScan("com.thinkxfactor")
+@EnableJpaRepositories(basePackages = "com.thinkxfactor")
+@EntityScan("com.thinkxfactor")
+@EnableSwagger2
 public class SpringdemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println(args);
 		SpringApplication.run(SpringdemoApplication.class, args);
 	}
+
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
 
 }
