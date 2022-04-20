f9ba47727b48b0911029fd69e889821d07971bae
SoumyadeepDatta <soumyadeepdatta34.alternative@gmail.com>
2022-04-20 11:23:52 +0530

    added findByISBN13

--- src/main/java/com/thinkxfactor/springdemo/repository/BookRepository.java
+++ src/main/java/com/thinkxfactor/springdemo/repository/BookRepository.java
@@ -1,9 +1,13 @@
 package com.thinkxfactor.springdemo.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.thinkxfactor.springdemo.entities.Book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book,Long>{
     
+    List<Optional<Book>> findByISBN13(long isbn13);
 }
